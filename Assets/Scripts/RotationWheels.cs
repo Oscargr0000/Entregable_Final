8578f82c3d1cd7a89cf035eab8424b7fb60f3a29
Oscargr0000 <oscargr0000@gmail.com>
2022-01-24 10:00:02 +0100

    día 1

---
+++ Assets/Scripts/RotationWheels.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationWheels : MonoBehaviour
+{
+    private float rotaionSpeed = 250f;
+    private float verticalInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Rotate(Vector3.right * rotaionSpeed * Time.deltaTime * verticalInput);
+    }
+}
