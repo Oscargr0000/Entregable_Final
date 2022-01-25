f3ffb40be688febf80b38330b15eeb85df17c590
Oscargr0000 <oscargr0000@gmail.com>
2022-01-25 01:31:27 +0100

    sdfcsadsa

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     private float rotationspeed = 100f;
     private float verticalInput;
     private float horizontalInput;
+    public GameObject projectilePrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -27,5 +28,10 @@ public class PlayerController : MonoBehaviour
         //Rotation movement
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Rotate(Vector3.up * rotationspeed * Time.deltaTime * horizontalInput);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 0.85f, gameObject.transform.position.z), gameObject.transform.rotation);
+        }
     }
 }
--- Assets/Scripts/RotationWheels.cs
+++ Assets/Scripts/RotationWheels.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RotationWheels : MonoBehaviour
 {
-    private float rotaionSpeed = 250f;
+    private float rotaionSpeed = 300f;
     private float verticalInput;
     // Start is called before the first frame update
     void Start()
