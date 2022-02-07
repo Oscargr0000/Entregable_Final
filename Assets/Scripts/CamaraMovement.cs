de8917c6c35381c3f20bdfa8a71ff0b8e9c06357
Oscargr0000 <oscargr0000@gmail.com>
2022-02-07 12:49:37 +0100

    fdfdf

--- Assets/Scripts/CamaraMovement.cs
+++ Assets/Scripts/CamaraMovement.cs
@@ -11,7 +11,12 @@ public class CamaraMovement : MonoBehaviour
     private Quaternion CamRotation_2 = Quaternion.Euler(30.975f, 73.261f, 0.065f);
     
     private Vector3 CamPosition_3 = new Vector3(30.48676f, 16.61137f, 158.7016f);
-    private Quaternion CamRotation_3 = Quaternion.Euler(20.49f, 80.469f, 0.062f);
+    private Quaternion CamRotation_3 = Quaternion.Euler(20.49f, 80.469f, 0.062f); 
+    
+    private Vector3 CamPosition_4 = new Vector3(110.7837f, 37.08178f, 151.1788f);
+    private Quaternion CamRotation_4 = Quaternion.Euler(45.242f, -148.973f, -0.027f);
+
+    //       rot:     
 
     private Vector3 SpawnCamara = new Vector3(0, 0, 0);
 
@@ -28,6 +33,9 @@ public class CamaraMovement : MonoBehaviour
     
     private float PlayerLimitZ_3 = 146f;
     private float PlayerLimitX_3 = 45f;
+    
+    private float PlayerLimitZ_4 = 135f;
+    private float PlayerLimitX_4 = 70f;
 
     // Start is called before the first frame update
     void Start()
@@ -61,6 +69,12 @@ public class CamaraMovement : MonoBehaviour
         {
             transform.position = CamPosition_3;
             transform.rotation = CamRotation_3;
+        }
+        
+        if (PlayerPosition.position.z <= PlayerLimitZ_4 && PlayerPosition.position.x >= PlayerLimitX_4)
+        {
+            transform.position = CamPosition_4;
+            transform.rotation = CamRotation_4;
         }
 
         
